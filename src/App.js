f537116e51017d6d781a2722c72c710497829069
wuming-233 <stevenchenzijian@gmail.com>
2021-06-12 20:25:32 -0400

    bruh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> lil nigga
+          Edit <code>src/App.js</code> lil
         </p>
       </header>
     </div>
