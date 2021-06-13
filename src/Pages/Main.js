8b8707f18b527eda57e8e935f4355d4472be7ea3
OE10x2 <oe10x2@gmail.com>
2021-06-13 10:08:50 -0400

    Tested Specific Anime Call

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import '../App.css';
 const jikanjs = require('jikanjs');
 
 class Main extends React.Component{
     state = {
         values: [],
     };
-    componentDidMount(){
-        jikanjs.loadUser('OE10x2', 'animelist').then((response) => {
-            response.anime.forEach(element => {
-                this.setState({values: [...this.state.values, element.title]});
-            })
-        }).catch((err) => {
-            console.error(err); // in case a error happens
-        });
+    async componentDidMount(){
+        //First load the userList
+        const userList = await jikanjs.loadUser('OE10x2', 'animelist');
+        //Only need "anime" component from userList
+        for (const element of userList.anime){
+            if (element.mal_id !== 9379){
+                this.setState({values: [...this.state.values, element]});
+            }else{
+                //Test if specific MAL ID can be recognized
+                const tempAnime = await jikanjs.loadAnime(element.mal_id);
+                //If so, load more information with "loadAnime" call with the ID
+                this.setState({values: [...this.state.values, tempAnime]});
+            }
+        }
     }
     render(){
         return(
             <React.Fragment>
                 <Typography variant="h1">TEST</Typography>
                 {this.state.values.map((value) => (
-                    <Typography key="value">{value}</Typography>
+                    //For "variant", just a test to see if the output is different
+                    <Typography key={value.mal_id} variant={value.mal_id !== 9379 ? "h5" : "body1"}>
+                        {/*To see if the result is different, output the synopsis for 9379*/}
+                        {value.mal_id !== 9379 ? value.title : value.synopsis}
+                    </Typography>
                 ))}
             </React.Fragment>
         );
