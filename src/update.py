5f4c73ceb3a8f6d3d92a4d3c9e352bdf4defd626
wuming-233 <stevenchenzijian@gmail.com>
2021-06-13 21:48:03 -0400

    put update underneath a function

--- src/update.py
+++ src/update.py
@@ -1,22 +1,23 @@
 from jikanpy import Jikan
 import json, time
 
-jikan = Jikan()
+def update():
+    jikan = Jikan()
 
-ez = jikan.user(username='OE10x2', request='animelist')
-raw_animes = ez['anime']
+    ez = jikan.user(username='OE10x2', request='animelist')
+    raw_animes = ez['anime']
 
-animes = []
-for i, anime in enumerate(raw_animes):
-    tmp = jikan.anime(anime['mal_id'])
-    lst = {}
-    lst['id'] = anime['mal_id']
-    lst['score'] = tmp['score']
-    lst['scored_by'] = tmp['scored_by']
-    lst['rank'] = tmp['rank']
-    animes.append(lst)
-    time.sleep(4)
-    print (f"""{i+1} out of {len(raw_animes)} done""")
+    animes = []
+    for i, anime in enumerate(raw_animes):
+        tmp = jikan.anime(anime['mal_id'])
+        lst = {}
+        lst['id'] = anime['mal_id']
+        lst['score'] = tmp['score']
+        lst['scored_by'] = tmp['scored_by']
+        lst['rank'] = tmp['rank']
+        animes.append(lst)
+        time.sleep(4)
+        print (f"""{i+1} out of {len(raw_animes)} done""")
 
-with open("animes.json", "w") as file:
-    json.dump(animes, file)
+    with open("animes.json", "w") as file:
+        json.dump(animes, file)
